e2dafa63b9d11df926a8db9aceb553e0f762337b
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2023-02-27 16:01:50 +0100

    new features

--- src/pages/api/city.js
+++ src/pages/api/city.js
@@ -31,7 +31,7 @@ export default async function searchLocations(searchCity) {
       url: "https://hotels4.p.rapidapi.com/locations/v3/search",
       params: { q: searchCity, locale: "en_US", currency: "USD" },
       headers: {
-        'X-RapidAPI-Key': '34674bdf14msh0be7f976c43b25fp191cd9jsne04e8bce6bbf',
+        'X-RapidAPI-Key': process.env.REACT_APP_PUBLIC_RAPIDAPI_KEY,
         'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
       }
     };
@@ -43,6 +43,7 @@ export default async function searchLocations(searchCity) {
     throw error;
   }
 }
+
 
 
 
