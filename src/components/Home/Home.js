ee35eccfadee3e2aab00c4adaa90a2c40520019c
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2023-02-27 01:46:14 +0100

    API fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,96 +1,106 @@
-import axios from 'axios';
-import React, {useState} from 'react';
+import React, { useState} from "react";
 import "./home.css";
-
+import searchLocations from "../../pages/api/city";
 
 const Home = () => {
-	const [searchCity, setSearchCity] = useState(null);
-	const [city, setCity] = useState(null);
-	const [checkIn, setCheckIn] = useState(null);
-	const [checkOut, setCheckOut] = useState(null);
-	const [guests, setGuests] = useState(null);
-	const [hotels, setHotels] = useState(null);
+  const [searchCity, setSearchCity] = useState("");
+  const [city, setCity] = useState(null);
+  const [checkIn, setCheckIn] = useState(null);
+  const [checkOut, setCheckOut] = useState(null);
+  const [guests, setGuests] = useState(null);
+  const [hotels, setHotels] = useState(null);
 
-	const getDestination = async () => {
-		try {
-			const res = await axios.get('api/city/', {
-				params: {searchCity} ,
-			});
-			const {data} = res;
+  const getDestination = async () => {
+    	try {
+		const result = await searchLocations(searchCity)
+		const randomIndex = Math.floor(Math.random() * result.sr.length);
+		const randomResult = result.sr[randomIndex];
+		setCity(randomResult.regionNames.fullName);
+	} catch (error) {
+		console.log(error.message);
+	} 
+  };
 
-			setCity(data.suggestions[0].entities[0].destinationId);
-		} catch (error) {
-			console.log(error.message);
-		}
-	};
+ /* console.log(city) */
 
-	const getHotels = async () => {
-		try {
-			const res = await axios.get('api/hotels/', {
-				params: {city, checkIn, checkOut, guests}
-			});
-			const {data} = res;
-			setHotels(data.data.body);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+
+
+  /*   const getHotels = async () => {
+    try {
+      const res = await axios.get("api/hotels/", {
+        params: { city, checkIn, checkOut, guests },
+      });
+      const { data } = res;
+      setHotels(data.data.body);
+    } catch (error) {
+      console.log(error);
+    }
+  }; */
   return (
     <div>
-      	<div className='search_box'>
-			<input
-				className='input_search'
-				type="text"
-				placeholder="Enter your destination city"
-				onChange={e => {
-					setCity(null);
-					setSearchCity(e.target.value);
-				}}
-			/>
-			<div className='search_btn'>
-				<button 
-					className='search_btn_spec'
-					onClick={() => getDestination()}
-				>
-					Search
-				</button>
-			</div>
-    	</div>
-		<div className='search_parms_container'>
-			<div className='search_parms'>
-				<div className='search_parms_details'>
-					<label for="check-in" className='label_content'> Check-in </label>
-					<input id="startDate" type="date"  onChange={e => setCheckIn(e.target.value)} />
-				</div>
-				<div className='search_parms_details'>
-					<label for="check-out" className='label_content'> Check-out </label>
-					<input id="check-out" type="date" onChange={e => setCheckOut(e.target.value)} />
-				</div>
-				<div className='search_parms_details'>
-					<label for="guests" className='label_content'> Guests </label>
-					<input
-						id="guests"
-						type="number"
-						placeholder="Total guests"
-						onChange={e => setGuests(e.target.value)}
-					/>
-				</div>
-				<div className='search_btn'>
-					<button
-						className='search_btn_spec' 
-						type="submit" 
-						onClick={() => getHotels()}
-					>
-						Find Hotels
-					</button>
-				</div>
-			</div>
-		</div>
-  		
-	</div>
+      <div className="search_box">
+        <input
+          className="input_search"
+          type="text"
+          placeholder="Enter your destination city"
+          onChange={(e) => setSearchCity(e.target.value)}
+          value={searchCity}
+        />
+        <div className="search_btn">
+          <button className="search_btn_spec" onClick={getDestination}>
+            Search
+          </button>
+        </div>
+      </div>
+      {/* <div className="search_parms_container">
+        <div className="search_parms">
+          <div className="search_parms_details">
+            <label for="check-in" className="label_content">
+              {" "}
+              Check-in{" "}
+            </label>
+            <input
+              id="startDate"
+              type="date"
+              onChange={(e) => setCheckIn(e.target.value)}
+            />
+          </div>
+          <div className="search_parms_details">
+            <label for="check-out" className="label_content">
+              {" "}
+              Check-out{" "}
+            </label>
+            <input
+              id="check-out"
+              type="date"
+              onChange={(e) => setCheckOut(e.target.value)}
+            />
+          </div>
+          <div className="search_parms_details">
+            <label for="guests" className="label_content">
+              {" "}
+              Guests{" "}
+            </label>
+            <input
+              id="guests"
+              type="number"
+              placeholder="Total guests"
+              onChange={(e) => setGuests(e.target.value)}
+            />
+          </div>
+          <div className="search_btn">
+            <button
+              className="search_btn_spec"
+              type="submit"
+              onClick={() => getHotels()}
+            >
+              Find Hotels
+            </button>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
-    
-  )
-}
-
-export default Home
+export default Home;
--- src/pages/api/city.js
+++ src/pages/api/city.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export default async function handler(req, res) {
+/* export default async function searchLocations(req, res) {
   const options = {
     method: "GET",
     url: "https://hotels4.p.rapidapi.com/locations/v3/search",
@@ -19,4 +19,30 @@ export default async function handler(req, res) {
     .catch(function (error) {
       console.error(error);
     });
+} */
+
+
+
+// API component
+export default async function searchLocations(searchCity) {
+  try {
+    const options = {
+      method: "GET",
+      url: "https://hotels4.p.rapidapi.com/locations/v3/search",
+      params: { q: searchCity, locale: "en_US", currency: "USD" },
+      headers: {
+        'X-RapidAPI-Key': '34674bdf14msh0be7f976c43b25fp191cd9jsne04e8bce6bbf',
+        'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+      }
+    };
+    const response = await axios.request(options);
+    console.log(response.status); // log the response status
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
+
+
+
